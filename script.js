7540e03654497923019f5f88fcc6ad25197a1e61
Kevin Li <kevinli617@gmail.com>
2023-05-16 22:55:31 -0400

    finished pokedex

--- script.js
+++ script.js
@@ -1,54 +1,90 @@
-// Declare document elements to variable names
-const photo = document.getElementById('mainphoto')
-const likes = document.getElementById('likes-display')
-const topName = document.getElementById('top-name')
-const bottomName = document.getElementById('bottom-name')
-const captionContent = document.getElementById('caption-content')
-const topCircle = document.getElementById('top-circle')
-const bottomCircle = document.getElementById('bottom-circle')
+const explorePage = document.querySelector('.explore-page')
+const profilePage = document.querySelector('.profile-page')
+const inputfield = document.getElementById('search')
+const profileGrids = document.querySelectorAll('.profile-grid-div')
+const profileSearchButton = document.getElementById('profile-bottom-search-button')
+const profilePicture = document.querySelector('.profile-picture')
+const profileTopName = document.querySelector('.profile-top-name')
+const profileBottomName = document.querySelector('.profile-bottom-name')
+const profileDescription = document.querySelector('.profile-description')
+const exploreGridDivs = document.querySelectorAll('.explore-grid-div')
 
+async function getDogPic() {
+    for (const grid of exploreGridDivs) {
+        let dog = await axios.get('https://dog.ceo/api/breeds/image/random')
+        let image = dog.data.message
+        // grid.innerHTML = `<img src=${image}/>`
+        grid.style.backgroundImage = `url(${image})`
+    }
+}
 
-// Add event listener for the input field
-
-const inputfield = document.querySelector('input')
+getDogPic()
 
 inputfield.addEventListener("keydown", async function(e) {
     if (e.key === "Enter") {
         let searchValue = inputfield.value.toLowerCase()
-        let pokemon = await fetchPokemon(searchValue)
-        renderProfile(pokemon)
+        renderProfile(searchValue)
+        inputfield.value = ''
     }
 })
 
-async function fetchPokemon(name) {
-    return await axios(`https://pokeapi.co/api/v2/pokemon/${name}`)
-}
+profileSearchButton.addEventListener('click', () => {
+    profilePage.style.display = "none";
+    explorePage.style.display = "block";
+})
 
-function renderProfile(pokemon) {
-    // destructure pokemon data, set photo
-    const {name, sprites, abilities, types} = pokemon.data
-    photo.setAttribute("src", sprites.front_default)
+async function renderProfile(pokemon) {
+    const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const pokemonData2 = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`)
 
-    // generate a random number of likes
-    const likesNumber = Math.floor(Math.random() * 1000)
-    likes.textContent = `${likesNumber} likes`
+    let string = pokemonData2.data.flavor_text_entries['0'].flavor_text
+    let sanitizedString = string.replace(/[\r\n]/gm, ' ')
 
-    // set the name at the top and bottom of page
-    topName.innerText = name
-    bottomName.textContent = name
+    let spritesObj = pokemonData.data.sprites
+    let spritesArr = Object.values(spritesObj)
+    let images = [];
 
-    // generate the caption with ability names
-    let caption = ''
-    caption += `#${types[0].type.name} `
-    for (const ability of abilities) {
-        caption += `#${ability.ability.name} `
+    profileTopName.innerText = pokemonData.data.name
+    profileBottomName.innerText = pokemonData.data.name
+    profileDescription.innerText = sanitizedString
+    profilePicture.innerHTML = `<img src="${spritesObj.front_default}")/>`
+
+    for (let i = 0; i< spritesArr.length; i++) {
+        if (spritesArr[i]) {
+            let str = spritesArr[i].toString()
+            if (str.includes("http")) {
+                images.push(spritesArr[i])
+            }
+        }
     }
-    captionContent.innerText = caption
 
-    // set the profile pictures
-    console.log(sprites.back_default)
-    topCircle.style.backgroundImage = `url(${sprites.back_default})`
-    bottomCircle.style.backgroundImage = `url(${sprites.back_default})`
+    function getRandomImage() {
+        const randomIndex = Math.floor(Math.random() * images.length)
+        const image = images[randomIndex]
+        return image;
+    }
 
-    console.log(pokemon.data)
+    function getRandomBackground() {
+        const backgrounds = [
+            './images/bgs/1.png',
+            './images/bgs/2.png',
+            './images/bgs/3.webp',
+            './images/bgs/4.webp',
+            './images/bgs/5.jpeg',
+            './images/bgs/6.jpeg'
+        ]
+        const randomIndex = Math.floor(Math.random() * backgrounds.length)
+        const bg = backgrounds[randomIndex]
+        return bg;
+    }
+    
+    profileGrids.forEach((grid) => {
+        grid.innerHTML = `<img src="${getRandomImage()}"/>`
+        grid.style.backgroundImage = `url(${getRandomBackground()})`
+    })
+
+    setTimeout(() => {
+        explorePage.style.display = "none";
+        profilePage.style.display = 'block';
+    }, 10)
 }
